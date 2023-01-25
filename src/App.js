e88045d32b12c5983e423449cb5b74380ccbd7bd
steverova <71293149+steverova@users.noreply.github.com>
2023-01-25 00:51:04 -0600

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
+import data from "./images";
+
+let files = null;
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState("");
+  const [modalState, setModalState] = useState(true);
+  const [modalData, setModalData] = useState("");
+
+  const onFileChange = (e) => {
+    files = e.target.files;
+    if (files.length !== 0) {
+      let fileReader = new FileReader();
+      fileReader.readAsDataURL(files[0]);
+      fileReader.onload = (event) => {
+        setSelectedImage(event.target.result);
+      };
+    }
+  };
+
+  const showModal = (data) => {
+    setModalState(false);
+    setModalData(data);
+    console.log(modalData);
+  };
+
+  const closeModal = () => {
+    setModalState(true);
+  };
+
+  const onSubmit = () => {
+    const formData = { image: selectedImage };
+    console.log(formData);
+
+    files = null;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App container-fluid">
+        <div className="col-md-7 mx-auto mt-5">
+          <div className="chooser_container">
+            <input type="file" onChange={onFileChange} />
+            <button className="btn" onClick={onSubmit}>
+              {" "}
+              <i class="fa-solid fa-file-arrow-up"></i>
+            </button>
+          </div>
+
+          <div className="gallery">
+            {data.map((item, index) => {
+              return (
+                <div key={index} class="card">
+                  <img src={item.image} alt="gallery" />
+                  <div className="button-menu">
+                    <button class="right-aligned-button btn-card">
+                      <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                    <button
+                      onClick={() => showModal(item.image)}
+                      class="right-aligned-button btn-card"
+                    >
+                      <i class="fa-solid fa-expand"></i>
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div hidden={modalState} className="gallery_modal">
+        <button className="btn-card m-5" onClick={() => closeModal()}>
+          <i class="fa-solid fa-compress"></i>
+        </button>
+
+        <div className="col-md-7 mx-auto">
+          <div className="modal_image_container">
+            <img src={modalData} alt="gallery" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/images.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    "image": "https://www.centauro.com.mx/wp-content/uploads/Sabes-que-esperar-al-tomarte-una-radiograf%C3%ADa-dental.jpg"
+  },
+  {
+    "image": "https://www.sonrisaparatodos.com/wp-content/uploads/2016/04/radiografia-panoramica-1024x559.jpg"
+  },
+  {
+    "image": "https://servibucal.es/wp-content/uploads/2017/05/radiografia-dental-oclusiva.jpg"
+  },
+  {
+    "image": "https://www.centauro.com.mx/wp-content/uploads/Sabes-que-esperar-al-tomarte-una-radiograf%C3%ADa-dental.jpg"
+  },
+  {
+    "image": "https://www.sonrisaparatodos.com/wp-content/uploads/2016/04/radiografia-panoramica-1024x559.jpg"
+  },
+  {
+    "image": "https://servibucal.es/wp-content/uploads/2017/05/radiografia-dental-oclusiva.jpg"
+  },
+
+  {
+    "image": "https://www.centauro.com.mx/wp-content/uploads/Sabes-que-esperar-al-tomarte-una-radiograf%C3%ADa-dental.jpg"
+  },
+  {
+    "image": "https://www.sonrisaparatodos.com/wp-content/uploads/2016/04/radiografia-panoramica-1024x559.jpg"
+  },
+  {
+    "image": "https://servibucal.es/wp-content/uploads/2017/05/radiografia-dental-oclusiva.jpg"
+  },
+  {
+    "image": "https://www.centauro.com.mx/wp-content/uploads/Sabes-que-esperar-al-tomarte-una-radiograf%C3%ADa-dental.jpg"
+  },
+  {
+    "image": "https://www.sonrisaparatodos.com/wp-content/uploads/2016/04/radiografia-panoramica-1024x559.jpg"
+  },
+  {
+    "image": "https://servibucal.es/wp-content/uploads/2017/05/radiografia-dental-oclusiva.jpg"
+  }
+]
+export default data;
